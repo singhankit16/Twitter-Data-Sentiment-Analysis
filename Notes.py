b3ee216104b6e3c060ebe24f67dbac0837abdd2d
singhankit16 <49933127+singhankit16@users.noreply.github.com>
2019-08-04 19:23:53 -0500

    Twitter Data Sentiment Analysis Code Upload
    Initial Upload

---
+++ Notes.py
@@ -0,0 +1,34 @@
+#The objective of this task is to detect hate speech in tweets. For the sake of simplicity, we say a tweet contains hate speech if it has a racist or sexist sentiment associated with it. So, the task is to classify racist or sexist tweets from other tweets.
+
+
+1) Understand the Problem Statement
+
+2) Tweets Preprocessing and Cleaning
+
+Data Inspection
+
+Data Cleaning
+
+
+3) Story Generation and Visualization from Tweets
+
+
+4) Extracting Features from Cleaned Tweets
+
+Bag-of-Words
+
+TF-IDF
+
+Word Embeddings
+
+
+5) Model Building: Sentiment Analysis
+
+Logistic Regression
+Support Vector Machine
+RandomForest
+XGBoost
+
+6) Model Fine-tuning
+
+7) Summary
